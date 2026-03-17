27292298957237735cb3fea5b8e84241889903e8
byte-ranajit <ranajitkhandual@gmail.com>
2026-03-17 11:48:29 +0530

    no changes

--- src/auth/PrivateRoute.jsx
+++ src/auth/PrivateRoute.jsx
@@ -1,8 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { isLoggedIn, isTokenExpired } from "./AuthUtils";
 
-function PrivateRoute({children}){
- return isLoggedIn() || isTokenExpired() ? children : <Navigate to="/login"/>
-}
+function PrivateRoute({ children }) {
 
+  if (!isLoggedIn() || isTokenExpired()) {
+    // logout(); // optional but recommended
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
 export default PrivateRoute;
